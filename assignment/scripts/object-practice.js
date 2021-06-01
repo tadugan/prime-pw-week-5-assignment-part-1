5353b7ef508dd935b4066863f3ff563029f7cb70
Tim Dugan <tadugan@me.com>
2021-06-01 13:52:51 -0500

    finished question 5, still need to double check work

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -53,3 +53,7 @@ console.log(me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'Blanched Almond';
+console.log(me.favoriteColor);
+console.log(me);
